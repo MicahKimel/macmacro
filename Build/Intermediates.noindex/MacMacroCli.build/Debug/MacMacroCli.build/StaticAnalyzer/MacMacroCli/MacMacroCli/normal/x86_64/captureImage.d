090048092a919ab73eba7c575e794e6cf4f28bd1
Micah Kimel <micah.kimel@my.wheaton.edu>
2024-12-02 13:42:47 -0500

    add .gitignore

--- Build/Intermediates.noindex/MacMacroCli.build/Debug/MacMacroCli.build/StaticAnalyzer/MacMacroCli/MacMacroCli/normal/x86_64/captureImage.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/micahkimel/Desktop/macmacro/MacMacroCli/captureImage.m \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/Foundation.framework/Modules/module.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/CoreGraphics.framework/Modules/module.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/ScreenCaptureKit.framework/Modules/module.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/AppKit.framework/Modules/module.modulemap
